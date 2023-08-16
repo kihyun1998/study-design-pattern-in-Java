5e7060c186f0730d1447963ffc00ec9bf7654791
penta-kihyun <71832733+kihyun1998@users.noreply.github.com>
2023-08-16 14:51:29 +0900

    observer

---
+++ obserber/src/classes/WeatherData.java
@@ -0,0 +1,47 @@
+package classes;
+
+import interfaces.Observer;
+import interfaces.Subject;
+
+import java.util.ArrayList;
+
+public class WeatherData implements Subject {
+    private ArrayList<Observer> observers;
+    private float temp;
+    private  float hum;
+    private  float pres;
+
+    public WeatherData(){
+        observers = new ArrayList<Observer>();
+    }
+
+    @Override
+    public void registerObserver(Observer o){
+        observers.add(o);
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+        observers.remove(o);
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (Observer observer: observers){
+            observer.update(temp,hum,pres);
+        }
+    }
+
+    public void measurementsChanged(){
+        notifyObservers();
+    }
+
+    public void setMeasurements(float temp, float hum, float pres){
+        this.temp = temp;
+        this.hum = hum;
+        this.pres = pres;
+        measurementsChanged();
+    }
+
+
+}
